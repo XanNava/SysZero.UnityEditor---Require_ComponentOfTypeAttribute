176052b60a4fef921f9a2cccc709d4552170343f
Alexander Nava <AlexanderXN78@Gmail.com>
2024-05-09 11:49:19 -0700

    Update Require.ComponentOfTypeAttribute.ExampleMain.cs

--- Require.ComponentOfTypeAttribute.ExampleMain.cs
+++ Require.ComponentOfTypeAttribute.ExampleMain.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public partial class Require_ComponentOfTypeAttribute_ExampleMain : MonoBehaviour {
 	[Require_ComponentOfType(typeof(ImInterfaceExample))]
 	public Component target;
+	private ImInterfaceExample _target;
+
+	// Used for internal assignment from external references.
+	public void Start() {
+		// Note the order, to prioritize the serialized reference on initialization.
+		if (target != null)
+			_target = (ImInterfaceExample)target;
+		if (target == null)
+			_target = GetComponentInChildren<ImInterfaceExample>();
+	}
+
+	[AddComponentMenu("Call()")]
+	public void Call() {
+		_target.DoSomething();
+	}
 }
